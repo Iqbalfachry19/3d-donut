cd6c9d4bac9173914519d6405ac1aed1d2ded890
iqbalfachry19 <ifachry@rocketmail.com>
2022-08-06 09:42:23 +0700

    hi

--- components/Upload.jsx
+++ components/Upload.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import { XIcon } from '@heroicons/react/outline';
+import { create } from 'ipfs-http-client';
 Modal.setAppElement('#__next');
 const customStyles = {
   content: {
@@ -17,10 +18,36 @@ const customStyles = {
     backgroundColor: 'rgba(10, 11, 13, 0.75)',
   },
 };
-const upload = (e) => {
-  e.preventDefault();
-};
+
 const Upload = ({ modalIsOpen, closeModal }) => {
+  const [selectedFile, setSelectedFile] = useState();
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsFilePicked(true);
+  };
+  const uploadIPFS = async (e) => {
+    e.preventDefault();
+
+    const reader = new window.FileReader();
+    reader?.readAsArrayBuffer(selectedFile);
+    reader.onloadend = async () => {
+      const buffer = Buffer.from(reader.result);
+      console.log(buffer);
+      const ipfs = create({ url: 'https://ipfs.infura.io:5001/api/v0' });
+      const result = await ipfs.add(buffer);
+      console.log(result);
+      closeModal();
+      if (
+        window.confirm(
+          'If you click "ok" you would be redirected . Cancel will load this website ',
+        )
+      ) {
+        window.location.href = `https://ipfs.infura.io/ipfs/${result.cid}?filename=${selectedFile.name}`;
+      }
+    };
+  };
+
   return (
     <Modal
       isOpen={modalIsOpen}
@@ -38,10 +65,32 @@ const Upload = ({ modalIsOpen, closeModal }) => {
         <label className="text-white">Model Description</label>
         <input className="text-black" />
         <label className="text-white">Model Image Url</label>
-        <input className="text-black" />
+        <input
+          type="file"
+          name="file"
+          onChange={changeHandler}
+          className="text-black"
+        />
+        {isFilePicked ? (
+          <div>
+            <p>Filename: {selectedFile.name}</p>
+            <p>Filetype: {selectedFile.type}</p>
+            <p>Size in bytes: {selectedFile.size}</p>
+            <p>
+              lastModifiedDate:{' '}
+              {selectedFile.lastModifiedDate.toLocaleDateString()}
+            </p>
+          </div>
+        ) : (
+          <p>Select a file to show details</p>
+        )}
         <label className="text-white">Model Url</label>
+
         <input className="text-black" />
-        <button onClick={(e) => upload(e)} className="bg-yellow-400 mt-2 p-2">
+        <button
+          onClick={(e) => uploadIPFS(e)}
+          className="bg-yellow-400 mt-2 p-2"
+        >
           Upload
         </button>
       </form>
